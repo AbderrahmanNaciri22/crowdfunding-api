6d7c2f30706ecae35e26616191e8069663080861
AbderrahmanNaciri22 <abderrhmanen2205@gmail.com>
2026-04-01 13:04:37 +0100

    add investment model

---
+++ src/models/investment.model.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const investmentSchema = new mongoose.Schema({
+  investor: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  project: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Project',
+    required: true
+  },
+  amount: {
+    type: Number,
+    required: true
+  },
+  percentage: {
+    type: Number,
+    required: true
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Investment', investmentSchema);
